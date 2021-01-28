b1c2b459bb822704020e73e6477aa34be8e310db
justyjaa.edu <justyjaa.edu@gmail.com>
2021-01-28 19:16:08 +0700

    Сделана 1 часть задачи

---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+    }
+}
--- CourseTasks/Range/RangeMain.cs
+++ CourseTasks/Range/RangeMain.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Range
+namespace RangeTask
 {
     class RangeMain
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите значения начала числового диапазона:");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значения конца числового диапазона:");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(from, to);
+
+            Console.WriteLine("Длина диапазона равна: " + range.GetLength());
+
+            Console.WriteLine("Введите число для проверки входа в диапазон: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (range.IsInside(number))
+            {
+                Console.WriteLine("Число входит в диапазон");
+            }
+            else
+            {
+                Console.WriteLine("Число не входит в диапазон");
+            }
         }
     }
 }
