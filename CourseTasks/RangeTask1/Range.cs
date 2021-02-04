1c7667adaa05e409970f23621d14c3514c6f9cc4
justyjaa.edu <justyjaa.edu@gmail.com>
2021-02-04 20:34:10 +0700

    Добавлена задача Range 1 часть

---
+++ CourseTasks/RangeTask1/Range.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RangeTask1
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+    }
+}
